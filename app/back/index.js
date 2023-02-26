ae50763ecddc5dc2be9f20b48a3cf52a519593c2
alex <fam231@yandex.ru>
2023-02-26 10:56:44 +0300

    Исправлен запрос

--- app/back/index.js
+++ app/back/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const mysql = require("mysql2");
+const Connection = require("mysql2/typings/mysql/lib/Connection");
 
 const connection = mysql.createConnection({
   host: "mysql",
@@ -46,54 +47,54 @@ const connection = mysql.createConnection({
 async function GetAllLists() {
   lists = { baseList: [], allList: [] };
   let sql = "SELECT * FROM lists ";
-  //   connection.connect(function (err) {
-  //     if (err) {
-  //       return console.error("Ошибка: " + err.message);
-  //     } else {
-  //       console.log("Подключение к серверу MySQL успешно установлено");
-  //     }
-  //   });
 
   connection.connect(function (err) {
-    if (err) throw console.error("Ошибка: " + err.message);
-    console.log("Подключение к серверу MySQL успешно установлено");
-    connection.query(sql, function (err, result) {
-      if (err) throw console.error("Ошибка: " + err.message);
-
-      console.log("Result: " + typeof result);
-
-      result.forEach((element) => {
-        switch (element.listName) {
-          case "baseList":
-            lists.baseList.push({
-              ElementName: element.item,
-              bay_state: element.state,
-            });
-            break;
-
-          default:
-            let indexList = lists.allList.indexOf(element.listName);
-            if (indexList < 0) {
-              lists.allList.push({
-                name: element.listName,
-                mas_elements: [
-                  { ElementName: element.item, bay_state: element.state },
-                ],
-              });
-            } else {
-              lists.allList[indexList].mas_elements.push({
-                ElementName: element.item,
-                bay_state: element.state,
-              });
-            }
-            break;
-        }
-        return lists;
-      });
-    });
-
-    // connection.end();
+    if (err) {
+      return console.error("Ошибка: " + err.message);
+    } else {
+      console.log("Подключение к серверу MySQL успешно установлено");
+    }
   });
+
+  let qry = await connection.query(sql, function (err, result) {
+    if (err) throw console.error("Ошибка: " + err.message);
+    console.log("Result: " + typeof result);
+    lists = result;
+  });
+
+  console.log("qry: ", typeof qry);
+  console.log("lists on GetAllLists:");
+  console.log(lists);
+
+  // connection.end();
+
+  // answ.forEach((element) => {
+  //         switch (element.listName) {
+  //           case "baseList":
+  //             lists.baseList.push({
+  //               ElementName: element.item,
+  //               bay_state: element.state,
+  //             });
+  //             break;
+
+  //           default:
+  //             let indexList = lists.allList.indexOf(element.listName);
+  //             if (indexList < 0) {
+  //               lists.allList.push({
+  //                 name: element.listName,
+  //                 mas_elements: [
+  //                   { ElementName: element.item, bay_state: element.state },
+  //                 ],
+  //               });
+  //             } else {
+  //               lists.allList[indexList].mas_elements.push({
+  //                 ElementName: element.item,
+  //                 bay_state: element.state,
+  //               });
+  //             }
+  //             break;
+  //         }
+  //     });
 }
 
 //Роуты
@@ -113,14 +114,16 @@ app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
 
-app.get("/lists", (req, res) => {
-  async function getDataLists(params) {
-    let answ = await GetAllLists();
-    console.log("answ: ");
-    console.log(answ);
-    return answ;
-  }
+app.get("/lists", async (req, res) => {
+  // async function getDataLists(params) {
+  //   let answ = await GetAllLists();
+  //   console.log("answ: ");
+  //   console.log(answ);
+
+  // return lists;
+  // }
+  await GetAllLists();
   console.log("lists in get: ");
-  console.log(getDataLists());
-  res.json(getDataLists());
+  console.log(lists);
+  res.json(lists);
 });
