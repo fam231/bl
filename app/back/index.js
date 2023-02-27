f7012370ecbb89da40a0a1c73c25a5b1c685350e
alex <fam231@yandex.ru>
2023-02-27 07:54:34 +0300

    Поправил запрос

--- app/back/index.js
+++ app/back/index.js
@@ -12,80 +12,40 @@ const connection = {
   connectionLimit: 10,
   queueLimit: 0,
 };
-// let lists = {
-//   baseList: [
-//     { ElementName: "Апельсин", bay_state: false },
-//     { ElementName: "Мандарин", bay_state: false },
-//     { ElementName: "Яблоко", bay_state: false },
-//   ],
-//   allList: [
-//     {
-//       name: "works",
-//       mas_elements: [
-//         { ElementName: "Сходить", bay_state: false },
-//         { ElementName: "Заказать", bay_state: false },
-//         { ElementName: "Забить", bay_state: false },
-//       ],
-//     },
-//     {
-//       name: "byus",
-//       mas_elements: [
-//         { ElementName: "Апельсин", bay_state: false },
-//         { ElementName: "Мандарин", bay_state: false },
-//         { ElementName: "Яблоко", bay_state: false },
-//       ],
-//     },
-//     {
-//       name: "date",
-//       mas_elements: [
-//         { ElementName: "11", bay_state: false },
-//         { ElementName: "12", bay_state: true },
-//         { ElementName: "13", bay_state: false },
-//       ],
-//     },
-//   ],
-// };
+let listsTmp = {
+  baseList: [
+    { ElementName: "Апельсин", bay_state: false },
+    { ElementName: "Мандарин", bay_state: false },
+    { ElementName: "Яблоко", bay_state: false },
+  ],
+  allList: [
+    {
+      name: "works",
+      mas_elements: [
+        { ElementName: "Сходить", bay_state: false },
+        { ElementName: "Заказать", bay_state: false },
+        { ElementName: "Забить", bay_state: false },
+      ],
+    },
+    {
+      name: "byus",
+      mas_elements: [
+        { ElementName: "Апельсин", bay_state: false },
+        { ElementName: "Мандарин", bay_state: false },
+        { ElementName: "Яблоко", bay_state: false },
+      ],
+    },
+    {
+      name: "date",
+      mas_elements: [
+        { ElementName: "11", bay_state: false },
+        { ElementName: "12", bay_state: true },
+        { ElementName: "13", bay_state: false },
+      ],
+    },
+  ],
+};
 
-// async function GetAllLists() {
-//   lists = { baseList: [], allList: [] };
-//   let sql = "SELECT * FROM lists ";
-
-//   // connection.connect(function (err) {
-//   //   if (err) {
-//   //     return console.error("Ошибка: " + err.message);
-//   //   } else {
-//   //     console.log("Подключение к серверу MySQL успешно установлено");
-//   //   }
-//   // });
-
-//   let qer = await pool.query(sql, function (err, result) {
-//     if (err) throw console.error("Ошибка: " + err.message);
-//     console.log("Result: " + typeof result);
-//     console.log("get result");
-//     return result
-//   });
-
-//   console.log("lists on GetAllLists:");
-//   console.log(await ans);
-
-//   return qer;
-
-//   // connection.end();
-
-//   // answ.
-
-//Роуты
-
-// async function StartApp(params) {
-//   try {
-//     let lists = await GetAllLists();
-//     console.log('lists: ', lists);
-
-//   } catch (error) {
-//     console.log("error: ", error);
-//   }
-// }
-// StartApp();
 async function GetAllLists() {
   let lists = { baseList: [], allList: [] };
   let sqlReq = "SELECT * FROM lists ";
@@ -143,5 +103,5 @@ app.listen(PORT, () => {
 });
 
 app.get("/lists", (req, res) => {
-  res.json(GetAllLists());
+  res.json(listsTmp);
 });
