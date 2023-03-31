661420d75a101bdada297b66b5e2e5063a179eaa
alex <fam231@yandex.ru>
2023-03-31 15:03:45 +0300

    connection us promise

--- app/back/index.js
+++ app/back/index.js
@@ -111,8 +111,8 @@ app.get("/lists", (req, res) => {
   connection
     .query(sqlReq, [2, 2])
     .then((result) => {
-      console.log("result: ");
-      console.log(result);
+      // console.log("result: ");
+      // console.log(result);
       result.forEach((element) => {
         switch (element.listName) {
           case "baseList":
