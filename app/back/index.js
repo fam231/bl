4cf883987c66eaecd5fa7eb0ff84060d4876bdf7
alex <fam231@yandex.ru>
2023-03-31 09:02:55 +0300

    connection us promise

--- app/back/index.js
+++ app/back/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const mysql = require("mysql2/promise");
+const mysql = require("mysql2");
 
 const connection = mysql
   .createConnection({
